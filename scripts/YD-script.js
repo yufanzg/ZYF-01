3ac0f90d9c03bfd84f95af74d5303abdb91d1648
yufanzg <126318783+yufanzg@users.noreply.github.com>
2023-02-26 08:45:42 +0800

    Update YD-script.js

--- scripts/YD-script.js
+++ scripts/YD-script.js
@@ -1,10 +1,10 @@
 let tu = [
-  "/images/崩坏3六周年.png",
-  "/images/a50f4bfbfbedab64dd1668b8f136afc379311e65.jpg",
-  "/images/5ecce26be24fb.jpg",
-  "/images/R-C (1).png",
-  "/images/R-C (2).png",
-  "/images/Hd086109cbe784d1b9156d335026b761f0.jpg",
+  "./images/崩坏3六周年.png",
+  "./images/a50f4bfbfbedab64dd1668b8f136afc379311e65.jpg",
+  "./images/5ecce26be24fb.jpg",
+  "./images/R-C (1).png",
+  "./images/R-C (2).png",
+  "./images/Hd086109cbe784d1b9156d335026b761f0.jpg",
 ];
 let i0 = 0;
 let img0 = document.getElementById("img0");
